93e2caec20d8ee89e895e7045cdca6d874dd9758
mouayadalhariri <70194387+mouayadalhariri@users.noreply.github.com>
2020-09-01 09:39:57 +0200

    error med DOM

--- programmering1/introkodning/index.js
+++ programmering1/introkodning/index.js
@@ -1,5 +1,16 @@
-console.log("Hej")
 
 // skapar en variabel som heter name och tilldelar det värdet "Mouayad"
-let name = "kokchun"
-console.log(`Hej ${Mouayad}`)
+let name = "Mouayad" // sträng
+let age = 18 // heltal, int
+
+console.log(`Hej ${name} du är ${age} år gammal`)
+
+if (age >= 18){
+    console.log("Du är vuxen")
+} else {
+    console.log("Du är barn")
+}
+
+// DOM - Document Object Model
+let text = document.querySelector("#text")
+text.innerHTML = `hej`
