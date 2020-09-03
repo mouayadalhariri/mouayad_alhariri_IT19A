add1a0f188a2a9437a19d8c83c17199402f60e03
mouayadalhariri <70194387+mouayadalhariri@users.noreply.github.com>
2020-09-03 09:56:02 +0200

    miniräknare v1

--- programmering1/introkodning/index.js
+++ programmering1/introkodning/index.js
@@ -14,3 +14,30 @@ if (age >= 18){
 // DOM - Document Object Model
 let text = document.querySelector("#text")
 text.innerHTML = `hej`
+
+// skapar variable för p-taggarna
+let mult = document.querySelector("#multiplikation")
+let div = document.querySelector("#division")
+let add = document.querySelector("#addition")
+let sub = document.querySelector("#subtraktion")
+
+mult.innerHTML = `multiplikation`
+div.innerHTML = `division`
+add.innerHTML = `addition`
+sub.innerHTML = `subtraktion`
+
+let button = document.querySelector("#knapp")
+
+// lägger till en eventlyssnare till knappen
+button.addEventListener("click", math)
+
+function math(event){
+    console.log("math-funktionen köra")
+
+    let num1 = parseFloat(document.querySelector("#number1").value)
+    let num2 = parseFloat(document.querySelector("#number2").value)
+   add.innerHTML = `${num1}+${num2}=${num1+num2} `
+   sub.innerHTML = `${num1}-${num2}=${num1-num2} `
+   mult.innerHTML = `${num1}*${num2}=${num1*num2} `
+   div.innerHTML = `${num1}/${num2}=${num1/num2} `
+}
