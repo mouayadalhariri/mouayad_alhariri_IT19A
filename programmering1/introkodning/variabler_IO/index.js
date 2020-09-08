d9464683f3a43731eb0de74d31ed94a0424be15f
mouayadalhariri <70194387+mouayadalhariri@users.noreply.github.com>
2020-09-08 09:39:48 +0200

    räknat medelvärde

---
+++ programmering1/introkodning/variabler_IO/index.js
@@ -0,0 +1,22 @@
+// skapa en variabel name och tilldela det ditt namn
+name = "Mouayad"
+
+
+//skriv ut "Hej namn"
+console.log (`Hej ${name}`)
+
+//1. skapa variablerna tal1, tal2, tal3
+//2. tilldela dessa variabler med värderna 3,4,5
+let tal1 = 3
+let tal2 = 4
+let tal3 = 5
+
+//3. räkna ut medelvärdet av dem
+medelvarde = (tal1+tal2+tal3)/3
+
+//4. gör följande utskrift:
+//"Medelvärdet av talen 3,4,5 är 4"
+console.log(`(${tal1}+${tal2}+${tal3})/3 = ${medelvarde}`)
+
+let langd = prompt("ange värdet för rektangelns längd ")
+
