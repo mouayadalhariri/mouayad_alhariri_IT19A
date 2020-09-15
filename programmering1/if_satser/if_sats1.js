8fde45ce57bfa1d4ca5f1a6730cdb2bc0a884d9a
mouayadalhariri <70194387+mouayadalhariri@users.noreply.github.com>
2020-09-15 09:39:19 +0200

    not done

--- programmering1/if_satser/if_sats1.js
+++ programmering1/if_satser/if_sats1.js
@@ -1,10 +1,8 @@
 // modulusoperator
 console.log(`${5%2}`)
 
-let tal = 20
-
 // avgöra om talet är jämnt eller udda
-
+let tal = 5
 // tal%2 är 0: jämnt
 // tal%2 är 1: udda
 
@@ -14,5 +12,15 @@ if (tal%2 === 0) {
     console.log("udda")
 }
 
+// uppgift
 // 1. låt användaren skriva in ett tal
 // 2. avgör om talet är positivt, 0 eller negativt
+let tal1 = prompt("Ange ett tal: ")
+
+if (tal1 > 0 ){
+    console.log("positivt")
+} else if (tal1 == 0){
+    console.log("Talet är 0")
+}else {
+    console.log("negativt")
+}
