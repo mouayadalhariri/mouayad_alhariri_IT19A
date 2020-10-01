ce063dd804959166642c5194a9e762fc5d3e3fb5
mouayadalhariri <70194387+mouayadalhariri@users.noreply.github.com>
2020-10-01 09:50:57 +0200

    DONE

---
+++ programmering1/forsats/for_sats.js
@@ -0,0 +1,38 @@
+let p_talserie1 = document.querySelector("#talserie1")
+let p_talserie2 = document.querySelector("#talserie2")
+let p_summa = document.querySelector("#summa")
+let p_klass = document.querySelector("#klasslista")
+let p_Udda = document.querySelector("#Udda")
+let p_Jämna = document.querySelector("#Jämna")
+let summa = 0
+
+Jämna.innerHTML = "Jämna tal från 0 till 100: ";
+for(let i=1; i<=100; i++){
+    if( i % 2==0){
+    Jämna.innerHTML += `${i},`;
+    }
+}
+Udda.innerHTML = "Udda tal från 0 till 100: "
+for(let j=1; j<=100; j++){
+    if(j % 2==1){
+        Udda.innerHTML += `${j},`;
+    }
+}
+
+
+
+for (let i = 0; i<10; i++){
+    console.log(`${i}. I will not write all over the walls`)
+}
+
+
+let ämnen = ["Svenska", "Engelska" , "Programmering" , "Matematik" , "Idrott"]
+
+// skapa en lista med lektioner
+
+
+p_klass.innerHTML = "Mina lektioner är <br/>"
+
+for (let k =0; k < ämnen.length; k++){
+    p_klass.innerHTML += `${k+1} ${ämnen[k]} <br/>`
+}
